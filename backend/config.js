9b02a44e3daa25261421809fef4b529c726a7479
NishantSwat <nayanishant.info@gmail.com>
2024-03-01 17:57:49 +0530

    Added dynamic route

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    // MONGODB_URL : 'mongodb://localhost:27017/twitter_clone',
     MONGODB_URL : 'mongodb://localhost:27017/twitter_clone',
     SECRET_KEY: 'Twitter-clone-miligram-JWT-secret-key-qwerty-poiuy-00998856@#$^%^&$^%^***^*///><?><>?{}{}:{:">:>IJP*('
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,22 @@
 import "./App.css";
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Login from "./pages/auth/Login";
-import Register from './pages/auth/Register';
-import Home from "./pages/home/Home";
-import Profile from './pages/profile/Profile'
+// import { Routes, Route } from "react-router-dom";
+// import Login from "./pages/auth/Login";
+// import Register from './pages/auth/Register';
+// import Home from "./pages/home/Home";
+// import Profile from './pages/profile/Profile'
+import DynamicRoute from "./DynamicRoute";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
+      <DynamicRoute />
+      {/* <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/" element = {<Home />} />
         <Route path="/profile" element={<Profile />} />
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
